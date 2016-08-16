37f8d8cf092c45d971c2cafd8e66bb844ef2f4c4
ymanseur <ymanseur94@gmail.com>
2016-08-16 11:50:26 -0400

    removed commented out code and fixed bg color

--- js/main.js
+++ js/main.js
@@ -1,60 +1,3 @@
-/*var scene, camera, clock, prev=0, showInstruct=true; startRendering=false;
-
-var moveLeft=false, moveRight=false;
-
-window.onload = function(){
-    initializeScene();
-    setScene();
-    animateScene();
-};
-
-function initializeScene(){
-    var canvasWidth = window.innerWidth;
-    var canvasHeight = window.innerHeight;
-    var aspectRatio = canvasWidth / canvasHeight;
-
-    renderer = new THREE.WebGLRenderer({antialias:true});
-    renderer.setClearColor(0xD3D3D3);
-    renderer.setSize(canvasWidth, canvasHeight);
-
-    document.getElementById("webgl-canvas").appendChild(renderer.domElement);
-
-    scene = new THREE.Scene();
-    camera = new THREE.PerspectiveCamera(45, aspectRatio, 1, 10000);
-    clock = new THREE.Clock();
-
-    addEventListeners();
-}
-
-function setScene() {
-    camera.position.set(0,150,150);
-    camera.lookAt(scene.position);
-    scene.add(camera);
-
-    loadBackdrop();
-}
-
-function animateScene() {
-    setTimeout( function() {
-        requestAnimationFrame(animateScene);
-    }, 1000 / 60);
-
-    if(startRendering)
-        renderScene();
-    renderer.render(scene, camera);
-    if(startRendering){
-        var fps = document.getElementById("fps");
-        fps.innerHTML = (1 / (elapsed - prev)).toFixed(2);
-        prev = elapsed;
-    }
-
-}
-
-function renderScene() {
-    elapsed = clock.getElapsedTime();
-}
-*/
-
 var game = (function () {
     var width = window.innerWidth;
     var height = window.innerHeight;
@@ -70,7 +13,7 @@ var game = (function () {
     var moveLeft = false;
     var moveRight = false;
 
-    renderer.setClearColor(0xD3D3D3);
+    renderer.setClearColor(0x000000);
     renderer.setSize(width, height);
 
     document.getElementById("webgl-canvas").appendChild(renderer.domElement);
