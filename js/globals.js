5b77b99dc6a68763023b0413eb5d9413f691ae85
ymanseur <ymanseur94@gmail.com>
2016-11-15 17:15:37 -0500

    Added global variable to hold spaceship color

--- js/globals.js
+++ js/globals.js
@@ -3,5 +3,6 @@ var Globals =
         BaseSize: 1,
         LaserColor: 0xFFFF00,
         LaserDelay: 10,
-        LaserIntensity: 2
+        LaserIntensity: 2,
+        ShipColor: 0x39FF14
     };
