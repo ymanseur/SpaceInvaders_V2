4de27bd88a3098bcfeb8b6038d184fd1dd1ad413
ymanseur <ymanseur94@gmail.com>
2016-11-16 15:36:59 -0500

    Created difficulty enum

--- js/globals.js
+++ js/globals.js
@@ -1,8 +1,9 @@
 var Globals =
     {
         BaseSize: 1,
-        EnemySpeed: 1,
+        EnemyCycleLength: 180,
         EnemyLaserSpeed: 4,
+        EnemySpeed: 0.5,
         LaserColor: 0xFFFF00,
         LaserDelay: 10,
         LaserIntensity: 2,
@@ -10,3 +11,10 @@ var Globals =
         PlayerSpeed: 4,
         ShipColor: 0x39FF14
     };
+
+var Difficulty =
+    {
+        Easy: 1, // Enemies move side to side
+        Medium: 2, // Enemies move in all 4 directions
+        Hard: 3 // Enemies move side to side while approaching player
+    }
